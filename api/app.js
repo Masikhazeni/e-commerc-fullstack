7e3139dea7e8ee6c659fac93ff137bfcffcde16d
Masikhazeni <masikhazeni210@gmail.com>
2025-04-26 19:44:20 +0430

    Adding the public folder and handling errors

--- api/app.js
+++ api/app.js
@@ -3,6 +3,8 @@ import morgan from 'morgan'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import cors from 'cors'
+import HandleERROR from './Utils/handleError.js'
+import catchError from './Utils/catchError.js'
 const __filename=fileURLToPath(import.meta.url)
 export const __dirname=path.dirname(__filename)
 
@@ -11,8 +13,15 @@ const app=express()
 app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
+app.use(express.static('Public'))
 
 
 
-
+app.use('*',(req,res,next)=>{
+    return next(new HandleERROR('route not found',404))
+ })
+ 
+ 
+ app.use(catchError)
+ 
 export default app
