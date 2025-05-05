b6ea6636f765f2a52c79b446f8ffb047b2106b78
Masikhazeni <masikhazeni210@gmail.com>
2025-05-05 17:00:52 +0430

    report and search section completed

--- api/Controllers/app.js
+++ api/Controllers/app.js
@@ -4,20 +4,21 @@ import path from "path";
 import catchError from "./Utils/catchError.js";
 import HandleERROR from "./Utils/handleError.js";
 import cors from "cors";
-import uploadRouter from './Routes/Upload.js'
+import authRouter from "./Routes/Auth.js";
+import uploadRouter from "./Routes/Upload.js";
+import userRouter from "./Routes/User.js";
 import { isLogin } from "./Middlewares/isLogin.js";
 import addressRouter from "./Routes/Address.js";
-import userRouter from "./Routes/User.js";
-import sliderRouter from "./Routes/Slider.js";
 import categoryRouter from "./Routes/Category.js";
+import brandRouter from "./Routes/Brand.js";
+import sliderRouter from "./Routes/Slider.js";
+import variantRouter from "./Routes/Variant.js";
 import productRouter from "./Routes/Products.js";
 import productVariantRouter from "./Routes/ProductVariant.js";
-import variantRouter from "./Routes/Variant.js";
 import commentRouter from "./Routes/Comment.js";
-import brandRouter from "./Routes/Brand.js";
 import discountRouter from "./Routes/DiscountCode.js";
+import cartRouter from "./Routes/Cart.js";
 import searchRouter from "./Routes/Search.js";
-
 
 const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);
@@ -25,16 +26,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("Public"));
-app.use('/api/address',isLogin,addressRouter)
+app.use('/api/auth',authRouter)
 app.use('/api/user',userRouter)
-app.use('/api/slider',sliderRouter)
+app.use('/api/address',isLogin,addressRouter)
 app.use('/api/category',categoryRouter)
+app.use('/api/brand',brandRouter)
+app.use('/api/slider',sliderRouter)
+app.use('/api/variant',variantRouter)
 app.use('/api/product',productRouter)
 app.use('/api/product-variant',productVariantRouter)
-app.use('/api/variant',variantRouter)
 app.use('/api/comment',commentRouter)
-app.use('/api/brand',brandRouter)
 app.use('/api/discount',discountRouter)
+app.use('/api/cart',isLogin,cartRouter)
 app.use('/api/search',searchRouter)
 app.use('/api/upload',uploadRouter)
 
