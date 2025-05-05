87867138bdc921eacb080be9f16f7c089d9afcad
Masikhazeni <masikhazeni210@gmail.com>
2025-05-05 16:28:45 +0430

    Carrying out the initial tasks to start the project

---
+++ api/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+import catchError from "./Utils/catchError.js";
+import HandleERROR from "./Utils/handleError.js";
+import cors from "cors";
+
+
+const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static("Public"));
+
+app.use('/api/upload',uploadRouter)
+
+app.use("*", (req, res, next) => {
+  next(new HandleERROR("Route not Found", 404));
+});
+app.use(catchError);
+
+export default app;
